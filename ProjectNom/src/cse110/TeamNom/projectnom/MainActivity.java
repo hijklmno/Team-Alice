ff5fe8ae9cd8444b709769f7f57cb5b6d67c37d0
Watson Yim <watson2272003@hotmail.com>
2014-04-27 16:17:04 -0700

    tabular methods - watson

--- ProjectNom/src/cse110/TeamNom/projectnom/MainActivity.java
+++ ProjectNom/src/cse110/TeamNom/projectnom/MainActivity.java
@@ -1,93 +1,90 @@
 package cse110.TeamNom.projectnom;
 
 import com.parse.Parse;
-
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import cse110.TeamNom.projectnom.adapter.TabsPagerAdapter;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-// testing committ raympndkjhklh
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
 
+	// Tab titles
+	private String[] tabs = { "Camera", "Profile", "Search", "News Feed" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Initialization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+            .setTabListener(this));
+		}
+		
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+		 
+		    @Override
+		    public void onPageSelected(int position) {
+		        // on changing the page
+		        // make respected tab selected
+		        actionBar.setSelectedNavigationItem(position);
+		    }
+		 
+		    @Override
+		    public void onPageScrolled(int arg0, float arg1, int arg2) {
+		    }
+		 
+		    @Override
+		    public void onPageScrollStateChanged(int arg0) {
+		    }
+		});
+	}
+
+	@Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
+ 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        
-        Parse.initialize(this, "d3r42N8HSeVbFU3RicMpjmjfgiRNHEKTOiSEcofn", "hsnuWjlI3k6gh19PrySFajFZwTvpuWwjbUZkFEo8");
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // on tab selected
+        // show respected fragment view
+        viewPager.setCurrentItem(tab.getPosition());
     }
-
-
+ 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
-    public void sendMessage(View view) {
-    	ParseObject testObject = new ParseObject("TestObject");
-    	testObject.put("foo", "bar");
-    	testObject.saveInBackground();
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
     }
     
-    public void getMessage(View view) {
-    	ParseQuery<ParseObject> query = ParseQuery.getQuery("TestObject");
-    	query.whereEqualTo("foo", "bar");
-//    	query.findInBackground(new FindCallback<ParseObject>() {
-//    	    public void done(List<ParseObject> scoreList, ParseException e) {
-//    	        if (e == null) {
-//    	            Log.d("score", "Retrieved " + scoreList.size() + " scores");
-//    	        } else {
-//    	            Log.d("score", "Error: " + e.getMessage());
-//    	        }
-//    	    }
-//    	});
-    }
+    
 }
-//HELLO
