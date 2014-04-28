e35da158bc2190a2dc6aee845e01d178cc10823a
Dave <d3ung@ucsd.edu>
2014-04-27 21:46:10 -0700

    Jean's Camera thing - David

--- ProjectNom/gen/cse110/TeamNom/projectnom/R.java
+++ ProjectNom/gen/cse110/TeamNom/projectnom/R.java
@@ -10,6 +10,24 @@ package cse110.TeamNom.projectnom;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,8 +36,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_action_favorite=0x7f020000;
@@ -31,12 +49,12 @@ public final class R {
         public static final int round_button=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int camera_photo_button=0x7f080003;
-        public static final int camera_preview=0x7f080001;
-        public static final int camera_surface_view=0x7f080002;
-        public static final int edit_message=0x7f080004;
-        public static final int pager=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int camera_photo_button=0x7f090003;
+        public static final int camera_preview=0x7f090001;
+        public static final int camera_surface_view=0x7f090002;
+        public static final int edit_message=0x7f090004;
+        public static final int pager=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,15 +65,15 @@ public final class R {
         public static final int fragment_search=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
-        public static final int app_id=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050002;
-        public static final int edit_message=0x7f050001;
-        public static final int title_activity_main=0x7f050004;
+        public static final int action_settings=0x7f060003;
+        public static final int app_id=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int button_send=0x7f060002;
+        public static final int edit_message=0x7f060001;
+        public static final int title_activity_main=0x7f060004;
     }
     public static final class style {
         /** 
@@ -79,10 +97,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f070000;
+        public static final int CustomActionBarTheme=0x7f070001;
+        /**  ActionBar styles 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int MyActionBar=0x7f070002;
+        /**  ActionBar tabs text styles 
+         */
+        public static final int MyActionBarTabText=0x7f070004;
+        /**  ActionBar title text 
+         */
+        public static final int MyActionBarTitleText=0x7f070003;
     }
 }
