e42889ef4c69122ade3a6c3d0e1110dca8cedec7
alicechen22193 <alicechen22193@gmail.com>
2014-05-04 16:00:00 -0700

    working nom and mmm button

--- ProjectNom/gen/cse110/TeamNom/projectnom/R.java
+++ ProjectNom/gen/cse110/TeamNom/projectnom/R.java
@@ -49,12 +49,15 @@ public final class R {
         public static final int round_button=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int Mmm=0x7f090007;
+        public static final int NOM=0x7f090006;
+        public static final int action_settings=0x7f090008;
         public static final int camera_photo_button=0x7f090003;
         public static final int camera_preview=0x7f090001;
         public static final int camera_surface_view=0x7f090002;
         public static final int edit_message=0x7f090004;
         public static final int pager=0x7f090000;
+        public static final int textView1=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- ProjectNom/src/cse110/TeamNom/projectnom/NewsFeedFragment.java
+++ ProjectNom/src/cse110/TeamNom/projectnom/NewsFeedFragment.java
@@ -1,19 +1,53 @@
 package cse110.TeamNom.projectnom;
 
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+
+
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class NewsFeedFragment extends Fragment {
+
+
+public class NewsFeedFragment extends Fragment implements OnClickListener{
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_newsfeed, container, false);
-        
+  
+//        Log.w("UD: ", "test");
+        final Button NOM = (Button) rootView.findViewById(R.id.NOM);    
+	     final Button Mmm = (Button) rootView.findViewById(R.id.Mmm); 
+	     NOM.setOnClickListener(this);
+	     Mmm.setOnClickListener(this);
+//	     Log.w("UD: ", "after");
         return rootView;
     }
+
+		public void onClick(View v) {
+			ParseObject testObject = new ParseObject("TestObject");
+			switch (v.getId()){
+			case R.id.NOM:
+			        testObject.put("NOM", 1);
+			        testObject.saveInBackground();
+			    break;
+			case R.id.Mmm:
+			        testObject.put("Mmm", 1);
+			        testObject.saveInBackground(); 
+				break;
+			}
+		}
+
+ 
 }
 
 
