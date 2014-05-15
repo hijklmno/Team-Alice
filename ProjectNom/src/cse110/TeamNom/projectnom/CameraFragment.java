ae4e71f1bd0f38353b21b4c4e55e300774a3c6e0
Willy <wihuang@ucsd.edu>
2014-05-14 20:29:49 -0700

    update with best camera yet

--- ProjectNom/src/cse110/TeamNom/projectnom/CameraFragment.java
+++ ProjectNom/src/cse110/TeamNom/projectnom/CameraFragment.java
@@ -1,121 +1,354 @@
 package cse110.TeamNom.projectnom;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 public class CameraFragment extends Fragment {
-	
+
 	// Activity request codes
-    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
-    public static final int MEDIA_TYPE_IMAGE = 1;
- 
-    // directory name to store captured images and videos
-    private static final String IMAGE_DIRECTORY_NAME = "Hello Camera";
- 
-    private Uri fileUri; // file url to store image/video
- 
-    private ImageView imgPreview;
-    private VideoView videoPreview;
-    private Button btnCapturePicture, btnRecordVideo;
-    
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	private static final int ACTION_TAKE_PHOTO_B = 1;
+
+	private Button picBtn;
+
+	private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+	private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
+	private ImageView mImageView;
+	private Bitmap mImageBitmap;
+
+
+	private String mCurrentPhotoPath;
+	private String pathtophoto;
+
+	private static final String JPEG_FILE_PREFIX = "IMG_";
+	private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+	private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
+
+	/* Photo album for this application */
+	private String getAlbumName() {
+		return "NOM";
+	}
+
+	private File getAlbumDir() {
+		File storageDir = null;
+
+		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+			storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
+
+			if (storageDir != null) {
+				if (! storageDir.mkdirs()) {
+					if (! storageDir.exists()){
+						Log.d("CameraSample", "failed to create directory");
+						return null;
+					}
+				}
+			}
+
+		} else {
+			Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+		}
+
+		return storageDir;
+	}
+
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+		File albumF = getAlbumDir();
+		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+		return imageF;
+	}
+
+	private File setUpPhotoFile() throws IOException {
+		File f = createImageFile();
+		mCurrentPhotoPath = f.getAbsolutePath();
+		return f;
+	}
+
+	private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+		int targetW = mImageView.getWidth();
+		int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+		int scaleFactor = 1;
+		if ((targetW > 0) || (targetH > 0)) {
+			scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+		}
+
+		/* Set bitmap options to scale the image decode target */
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+		mImageView.setImageBitmap(bitmap);
+		mImageView.setVisibility(View.VISIBLE);
+	}
+
+	private void galleryAddPic() {
+		Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+		File f = new File(mCurrentPhotoPath);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		getActivity().sendBroadcast(mediaScanIntent);
+	}
+
+	private void dispatchTakePictureIntent(int actionCode) {
+
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if(actionCode == ACTION_TAKE_PHOTO_B){
+			File f = null;
+			try {
+				f = setUpPhotoFile();
+				mCurrentPhotoPath = f.getAbsolutePath();
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+			} catch (IOException e) {
+				e.printStackTrace();
+				f = null;
+				mCurrentPhotoPath = null;
+			}
+		}
+		startActivityForResult(takePictureIntent, actionCode);
+	}
+
+	private void handleBigCameraPhoto() {
+		if (mCurrentPhotoPath != null) {
+			setPic();
+			galleryAddPic();
+			pathtophoto = mCurrentPhotoPath;
+			mCurrentPhotoPath = null;
+		}
+	}
+
+	Button.OnClickListener mTakePicOnClickListener =
+			new Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+		}
+	};
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		Parse.initialize(getActivity(), "k6xrLx1ka30TdyjSmZZRF2XVkyrvEJJq38YtZbKW", "KTchPGVBZhFSaCOetY7XbBWyaQN262o2T04b60RC");
 
-        View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
-         
- //       imgPreview = (ImageView) rootView.findViewById(R.id.imgPreview);
-      //   videoPreview = (VideoView) findViewById(R.id.videoPreview);
-      btnCapturePicture = (Button) rootView.findViewById(R.id.btnCapturePicture);
-      //   btnRecordVideo = (Button) findViewById(R.id.btnRecordVideo);
- 
-        System.out.println("Check point 1");
-        /**
-         * Capture image button click event
-         */
-        btnCapturePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // capture picture
-                captureImage();
-                previewCapturedImage();
-            }
-        });
-        System.out.println("Check point 2 ");
-        // Checking camera availability
-        if (!isDeviceSupportCamera()) {
-            Toast.makeText( super.getActivity().getApplicationContext(),
-                    "Sorry! Your device doesn't support camera",
-                    Toast.LENGTH_LONG).show();
-            // will close the app if the device does't have camera
-            super.getActivity().finish();
-        }
-        
-        return rootView;
-    }
-	
-	
-	/**
-     * Checking device has camera hardware or not
-     */
-    private boolean isDeviceSupportCamera() {
-        if (super.getActivity().getApplicationContext().getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA)) {
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-    
-	public void captureImage() {
-		Intent intent = new Intent(super.getActivity(), CameraActivity.class);
-		
-		startActivity(intent);
+		View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
 
-	} 
-	
+		picBtn = (Button) rootView.findViewById(R.id.btnCapturePicture);
 
-    /**
-     * Display image from a path to ImageView
-     */
-    private void previewCapturedImage() {
-        try {
-            // hide video preview
-            videoPreview.setVisibility(View.GONE);
- 
-            imgPreview.setVisibility(View.VISIBLE);
- 
-            // bimatp factory
-            BitmapFactory.Options options = new BitmapFactory.Options();
- 
-            // downsizing image as it throws OutOfMemory Exception for larger
-            // images
-            options.inSampleSize = 8;
- 
-            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
-                    options);
- 
-            imgPreview.setImageBitmap(bitmap);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
+		System.out.println("Check point 1");
+		/**
+		 * Capture image button click event
+		 */
+		picBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				mImageView = (ImageView) getActivity().findViewById(R.id.imageView1);
+				mImageBitmap = null;
+
+				setBtnListenerOrDisable(
+						picBtn,
+						mTakePicOnClickListener,
+						MediaStore.ACTION_IMAGE_CAPTURE
+						);
+
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+					mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
+				} else {
+					mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+				}
+
+				dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+			}
+		});
+		System.out.println("Check point 2 big boiiii ");
+		// Checking camera availability
+		if (!isDeviceSupportCamera()) {
+			Toast.makeText( super.getActivity().getApplicationContext(),
+					"Sorry! Your device doesn't support camera",
+					Toast.LENGTH_LONG).show();
+			// will close the app if the device does't have camera
+			super.getActivity().finish();
+		}
+		return rootView;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == ACTION_TAKE_PHOTO_B && resultCode == -1) {
+			// Work with this
+			handleBigCameraPhoto();
+			try {
+				// uncomment this to send photo to parse, ask william or david
+				compressFile(pathtophoto);
+				System.out.println("The photopath: " + pathtophoto);
+				//finish();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("badasssssssswholes");
+	}
+
+	// Some lifecycle callbacks so that the image can survive orientation change
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
+		super.onSaveInstanceState(outState);
+	}
+
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
+		mImageView.setImageBitmap(mImageBitmap);
+		mImageView.setVisibility(
+				savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ?
+						ImageView.VISIBLE : ImageView.INVISIBLE
+				);
+	}
+
+	public static boolean isIntentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list =
+				packageManager.queryIntentActivities(intent,
+						PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+
+	/* Method that compresses a file given a path. */
+	private void compressFile(String path) throws Exception{
+		try {
+			System.out.println("inside the compress method: " + path);
+			Bitmap bitmap;
+			bitmap = BitmapFactory.decodeFile(path);
+			if(bitmap == null)
+			{
+				System.out.println(path + "cannot be converted to a bitmap!");
+				return;
+			}
+			Bitmap bmpCompressed = Bitmap.createScaledBitmap(bitmap, 400, 400, true);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			bmpCompressed.compress(CompressFormat.JPEG, 100, bos);
+			byte[] data = bos.toByteArray();
+			//uncompressFile(data);
+
+			System.out.println("Before parsing");
+			ParseObject object = new ParseObject("Food_Table_DB");
+			ParseFile file = new ParseFile(data);
+			file.saveInBackground(new SaveCallback() {
+				@Override
+				public void done(ParseException e) {
+					// TODO Auto-generated method stub
+					if (e == null) {
+
+					}
+					else {
+						System.err.println("ParseException: " + e);
+					}
+				}
+			});
+
+			object.put("Food_photo", file);
+			object.put("Like", 1);
+			object.put("Bookmark", 1);
+			object.saveInBackground();
+			System.out.println("after parsing blue balls");
+		}
+		catch (Exception e) {
+			Log.v("CameraFragment", "Some error compressFile");
+		}
+		// clear the path string
+		//pathtophoto = null;
+	}
+
+	// this method is for... dont trip 
+	private void uncompressFile(byte array[])
+	{
+		System.out.println("heeloeeellelelele");
+		Bitmap bmp = BitmapFactory.decodeByteArray(array, 0, array.length);
+		/* Associate the Bitmap to the ImageView */
+		mImageView.setImageBitmap(bmp);
+		mImageView.setVisibility(View.VISIBLE);
+	}
+
+	private boolean isDeviceSupportCamera() {
+		if (super.getActivity().getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
+	}
+
+	private void setBtnListenerOrDisable(
+			Button btn,
+			Button.OnClickListener onClickListener,
+			String intentName
+			) {
+		if (isIntentAvailable(super.getActivity().getApplicationContext(), intentName)) {
+			btn.setOnClickListener(onClickListener);
+		} else {
+			btn.setText(
+					getText(R.string.cannot).toString() + " " + btn.getText());
+			btn.setClickable(false);
+		}
+	}
 }
