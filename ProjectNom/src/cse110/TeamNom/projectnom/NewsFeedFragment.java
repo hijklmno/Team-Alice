38341384f9f155a9a8eea85d768828831b21244b
alicechen22193 <alicechen22193@gmail.com>
2014-05-21 22:04:59 -0700

    working buttons on newsfeed

--- ProjectNom/src/cse110/TeamNom/projectnom/NewsFeedFragment.java
+++ ProjectNom/src/cse110/TeamNom/projectnom/NewsFeedFragment.java
@@ -1,12 +1,16 @@
 package cse110.TeamNom.projectnom;
 
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
 import cse110.TeamNom.projectnom.newsfeedadapter.CustomListAdapter;
 import cse110.TeamNom.projectnom.newsfeedadapter.NewsItem;
 import android.os.Bundle;
@@ -16,58 +20,85 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.view.View.OnClickListener;
 
 public class NewsFeedFragment extends Fragment implements OnClickListener{
-	private static final int MAXROWS = 2;
-
-	@Override
+private static final int MAXROWS = 2;
+private Button buttonNOM;
+@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_newsfeed, container, false);
 //      Log.w("UD: ", "test");
-//        final Button NOM = (Button) rootView.findViewById(R.id.NOM);    
+//        final Button NOM = (Button) rootView.findViewWithTag(newsItem.getReporterName());
+        
+//        if (NOM == null) {
+//        	Log.d("NULLLELD", "FSDFSDF");
+//        
+//        }
+        //sadf
 //	    final Button Mmm = (Button) rootView.findViewById(R.id.Mmm); 
-//	    NOM.setOnClickListener(this);
-//	    Mmm.setOnClickListener(this);
+//	    Mmm.setText("asdf");
+//	    NOM.setOnClickListener(new OnClickListener() {           
+//
+//	    	  @Override
+//	    	  public void onClick(View v) 
+//	    	  {
+//	    	      Log.d("NewsFeed", "WorkingNOM");
+//	    	  }    
+//	    	});
+//	    Mmm.setOnClickListener(new OnClickListener() {           
+//
+//	    	  @Override
+//	    	  public void onClick(View v) 
+//	    	  {
+//	    	  Log.d("NewsFeed", "WorkingMmm");
+//	    	  }    
+//	    	});
 ////	    Log.w("UD: ", "after");
-//	     
-//	     
-//	     
-	    ArrayList<NewsItem> image_details = getListData();
-		final ListView lv1 = (ListView) rootView.findViewById(R.id.custom_list);
-		lv1.setAdapter(new CustomListAdapter(getActivity(), image_details));
-		lv1.setOnItemClickListener(new OnItemClickListener() {
+   
+      //  View newView = inflater.inflate(R.layout.newsfeed_list_row_layout, container, false);
+      //  buttonNOM = (Button) newView.findViewById(R.id.NOM);
+        
+        ArrayList<NewsItem> image_details = getListData();
+   
+        final ListView lv1 = (ListView) rootView.findViewById(R.id.custom_list);
+        lv1.setAdapter(new CustomListAdapter(getActivity(), image_details));
+        lv1.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-				Object o = lv1.getItemAtPosition(position);
-				NewsItem newsData = (NewsItem) o;
-				Toast.makeText(getActivity(), "Selected :" + " " + newsData,
-						Toast.LENGTH_LONG).show();
-			}
+        	@Override
+        	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+        	Object o = lv1.getItemAtPosition(position);
+        	NewsItem newsData = (NewsItem) o;
+        	Toast.makeText(getActivity(), "Selected :" + " " + newsData,
+        	Toast.LENGTH_LONG).show();
+        	}
 
-		});
-		
-		
-		
+        });
+/**	 buttonNOM.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+ParseObject testObject = new ParseObject("watson");
+testObject.put("NOM", 1);
+           testObject.saveInBackground();
+}
+});
+*/
         return rootView;
     }
 
-	
-	
 	private ArrayList<NewsItem> getListData() {
 		ArrayList<NewsItem> results = new ArrayList<NewsItem>();
-//		Log.w("asdf", "asd");
+//	 Log.w("asdf", "asd");
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("watson");
 		query.setLimit(MAXROWS);
 		query.orderByAscending("updatedAt");
 		List<ParseObject> news;
 		try {
 			news = query.find();
-			
 			for (int i = 0; i < news.size(); i++) {
 				NewsItem newsData = new NewsItem();
 				newsData.setHeadline(news.get(i).getString("headline"));
@@ -75,33 +106,30 @@ public class NewsFeedFragment extends Fragment implements OnClickListener{
 				newsData.setDate(news.get(i).getString("date"));
 				newsData.setUrl(news.get(i).getString("url"));
 				results.add(newsData);
-//				Log.w("populateData", news.get(i).getString("headline"));
-				
-			}
-			
-		} catch (ParseException e) {
+//	 Log.w("populateData", news.get(i).getString("headline"));
+	}
+	} catch (ParseException e) {
 			Log.d("newsfeed", "Error: " + e.getMessage());
 		}
 
 		return results;
 	}
-		
-		public void onClick(View v) {
-			ParseObject testObject = new ParseObject("TestObject");
-			switch (v.getId()){
-			case R.id.NOM:
-			        testObject.put("NOM", 1);
-			        testObject.saveInBackground();
-			    break;
-			case R.id.Mmm:
-			        testObject.put("Mmm", 1);
-			        testObject.saveInBackground(); 
-				break;
-			}
+
+	public void onClick(View v) {
+		ParseObject testObject = new ParseObject("watson");
+		switch (v.getId()){
+	case R.id.NOM:
+       testObject.put("NOM", 1);
+       testObject.saveInBackground();
+       break;
+	case R.id.Mmm:
+       testObject.put("Mmm", 1);
+       testObject.saveInBackground(); 
+       	break;
 		}
+	}
 
  
 }
-
 
 
--- ProjectNom/src/cse110/TeamNom/projectnom/newsfeedadapter/CustomListAdapter.java
+++ ProjectNom/src/cse110/TeamNom/projectnom/newsfeedadapter/CustomListAdapter.java
@@ -2,8 +2,13 @@ package cse110.TeamNom.projectnom.newsfeedadapter;
 
 import java.util.ArrayList;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import cse110.TeamNom.projectnom.R;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,61 +21,84 @@ import android.widget.TextView;
 
 public class CustomListAdapter extends BaseAdapter {
 
-	private ArrayList<NewsItem> listData;
+private ArrayList<NewsItem> listData;
 
-	private LayoutInflater layoutInflater;
+private LayoutInflater layoutInflater;
 
-	public CustomListAdapter(Context context, ArrayList<NewsItem> listData) {
-		this.listData = listData;
-		layoutInflater = LayoutInflater.from(context);
-	}
+public CustomListAdapter(Context context, ArrayList<NewsItem> listData) {
+this.listData = listData;
+layoutInflater = LayoutInflater.from(context);
+}
 
-	@Override
-	public int getCount() {
-		return listData.size();
-	}
+@Override
+public int getCount() {
+return listData.size();
+}
 
-	@Override
-	public Object getItem(int position) {
-		return listData.get(position);
-	}
+@Override
+public Object getItem(int position) {
+return listData.get(position);
+}
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+@Override
+public long getItemId(int position) {
+return position;
+}
 
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		if (convertView == null) {
-			convertView = layoutInflater.inflate(R.layout.newsfeed_list_row_layout, null);
-			holder = new ViewHolder();
-			holder.headlineView = (TextView) convertView.findViewById(R.id.title);
-			holder.reporterNameView = (TextView) convertView.findViewById(R.id.reporter);
-			holder.reportedDateView = (TextView) convertView.findViewById(R.id.date);
-			holder.imageView = (ImageView) convertView.findViewById(R.id.thumbImage);
-			holder.mmm = (Button) convertView.findViewById(R.id.Mmm);
-			holder.nom = (Button) convertView.findViewById(R.id.NOM);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
+public View getView(int position, View convertView, ViewGroup parent) {
+final ViewHolder holder;
+if (convertView == null) {
+convertView = layoutInflater.inflate(R.layout.newsfeed_list_row_layout, null);
+holder = new ViewHolder();
+holder.headlineView = (TextView) convertView.findViewById(R.id.title);
+holder.reporterNameView = (TextView) convertView.findViewById(R.id.reporter);
+holder.reportedDateView = (TextView) convertView.findViewById(R.id.date);
+holder.imageView = (ImageView) convertView.findViewById(R.id.thumbImage);
+holder.mmm = (Button) convertView.findViewById(R.id.Mmm);
+holder.nom = (Button) convertView.findViewById(R.id.NOM);
+convertView.setTag(holder);
+} else {
+holder = (ViewHolder) convertView.getTag();
+}
+
+NewsItem newsItem = (NewsItem) listData.get(position);
+
+holder.headlineView.setText(newsItem.getHeadline());
+holder.reporterNameView.setText("By, " + newsItem.getReporterName());
+holder.reportedDateView.setText(newsItem.getDate());
+holder.mmm.setTag(newsItem.getReporterName());
+holder.mmm.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+ParseObject testObject = new ParseObject("watson");
+testObject.put("MmmString", holder.mmm.getTag());
+           testObject.saveInBackground();
+}
+});
+	holder.nom.setTag(newsItem.getReporterName());
+	holder.nom.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+	ParseObject testObject = new ParseObject("watson");
+	testObject.put("NomString", holder.nom.getTag());
+           testObject.saveInBackground();
 		}
+	});
 
-		NewsItem newsItem = (NewsItem) listData.get(position);
+// holder.imageView.setImageBitmap(uncompressImage(image));
 
-		holder.headlineView.setText(newsItem.getHeadline());
-		holder.reporterNameView.setText("By, " + newsItem.getReporterName());
-		holder.reportedDateView.setText(newsItem.getDate());
-		holder.mmm.setTag(newsItem.getReporterName());
-		holder.nom.setTag(newsItem.getReporterName());
-
-//		if (holder.imageView != null) {
-//			new ImageDownloaderTask(holder.imageView).execute(newsItem.getUrl());
-//		}
+//	 if (holder.imageView != null) {
+//	 new ImageDownloaderTask(holder.imageView).execute(newsItem.getUrl());
+//	 }
 
 		return convertView;
-	}
+		}
 
+	private Bitmap uncompressImage(byte array[]) {
+	System.out.println("heeloeeellelelele");
+	Bitmap bmp = BitmapFactory.decodeByteArray(array, 0, array.length);
+	return bmp;
+	}
 	static class ViewHolder {
 		TextView headlineView;
 		TextView reporterNameView;
