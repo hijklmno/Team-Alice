0e973c5a1346f1f29c955ad64572d2f48c0aad9b
Willy <wihuang@ucsd.edu>
2014-05-26 17:46:55 -0700

    made the UI beautiful - william

--- ProjectNom/gen/cse110/TeamNom/projectnom/R.java
+++ ProjectNom/gen/cse110/TeamNom/projectnom/R.java
@@ -468,15 +468,6 @@ containing a value of this type.
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -488,17 +479,17 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080003;
-        public static final int CustomActionBarTheme=0x7f080004;
+        public static final int AppBaseTheme=0x7f080007;
+        public static final int CustomActionBarTheme=0x7f080003;
         /**  ActionBar styles 
          */
-        public static final int MyActionBar=0x7f080005;
+        public static final int MyActionBar=0x7f080004;
         /**  ActionBar tabs text styles 
          */
-        public static final int MyActionBarTabText=0x7f080007;
+        public static final int MyActionBarTabText=0x7f080006;
         /**  ActionBar title text 
          */
-        public static final int MyActionBarTitleText=0x7f080006;
+        public static final int MyActionBarTitleText=0x7f080005;
         public static final int com_facebook_loginview_default_style=0x7f080000;
         public static final int com_facebook_loginview_silver_style=0x7f080001;
         public static final int tooltip_bubble_text=0x7f080002;
