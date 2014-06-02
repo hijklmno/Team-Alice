5ee18bacfa62cddfd81d52f365c4f86cfd440ccd
jyp033 <jyp033@ucsd.edu>
2014-06-01 22:29:07 -0700

    DAT PROFILE DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOEEEEEEEEEEEEEEEEEEEEEEE

---
+++ ProjectNom/src/cse110/TeamNom/projectnom/ImageAdapter.java
@@ -0,0 +1,51 @@
+package cse110.TeamNom.projectnom;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+	
+	private Context mContext;
+	 
+	private Drawable[] mThumbIds;
+	
+    // Constructor
+    public ImageAdapter(Context c, Drawable[] d){
+        mContext = c;
+        mThumbIds = d;
+    }
+ 
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+ 
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+ 
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+ 
+    @SuppressLint("NewApi")
+	@Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        //imageView.setImageResource(mThumbIds[position]);
+        //imageView.setBackground(mThumbIds[position]);
+        imageView.setImageDrawable(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+       // imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+        return imageView;
+
+    }
+
+}
